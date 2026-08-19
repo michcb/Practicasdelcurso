4ad7325eb98d872f9bc4af2f5d941cffbee987ef
michcb <76888416+michcb@users.noreply.github.com>
2026-08-18 21:39:30 -0300

    Add files via upload

--- modulo2_unidad1_diseno.sql
+++ modulo2_unidad1_diseno.sql
@@ -1,12 +1,12 @@
-CREATE TABLE Clientes(
-ID_cliente INT not null IDENTITY(1;1) PRIMARY KEY, -- numero entero y unico para identificar cada cliente. Not null porque ningun cliente puede existir sin este numero
-Nombre varchar(100) not null, -- texto de 100 caracteres para escribir nombre y not null porque si os i debe completarse este campo
-Perfil_bio TEXT, -- texto para detallar el perfil
-Fecha_registro date) -- date porque solo es con fecha, no con horarios.
+create table clientes(
+id_cliente int not null identity(1,1) primary key, -- numero entero y unico para identificar cada cliente. Not null porque ningun cliente puede existir sin este numero
+nombre varchar(100) not null, -- texto de 100 caracteres para escribir nombre y not null porque si os i debe completarse este campo
+perfil_bio text, -- texto para detallar el perfil
+fecha_registro date) -- date porque solo es con fecha, no con horarios.
 
 
-CREATE TABLE Productos(
-ID_producto INT not null IDENTITY(1;1) PRIMARY KEY, -- numero entero y unico para identificar cada producto. Not null porque ningun producto puede existir sin este numero.
+create table productos(
+id_producto int not null identity(1,1) primary key, -- numero entero y unico para identificar cada producto. Not null porque ningun producto puede existir sin este numero.
 descripcion varchar(255) not null, -- varchar para texto de 255 caracteres. nor null porque no puede estrar vacio.
-precio DECIMAL(10,2) not null, -- decimal porque admite hasta 10 enteros y 2 decimales. not null porque ningun producto puede estar sin precio.
-esta_activo varchar(2) NOT NULL, -- varchard con 2 caracteres para identificar con SI o NO. Not null porque no puede estar vacio este campo.
+precio decimal(10,2) not null, -- decimal porque admite hasta 10 enteros y 2 decimales. not null porque ningun producto puede estar sin precio.
+esta_activo varchar(2) not null, -- varchard con 2 caracteres para identificar con SI o NO. Not null porque no puede estar vacio este campo.
